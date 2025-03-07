60593b884f590ae8e8bae6cbcb13f1b8f1194762
Rao Naveed <oyerao18@gmail.com>
2025-03-07 16:29:52 +0500

    update main function to implement number guessing prompt and input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+use std::io;
+
 fn main() {
-    println!("Naveed Pasha!");
+    println!("Guess a Number!");
+
+    println!("Please input your guess.");
+
+     let mut guess = String::new();
+
+     io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("you guessed: {}",guess);
+
+
+
 }

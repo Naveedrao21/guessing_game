a73383687bd15cb2127f5d3c68a00813384f9b23
Rao Naveed <oyerao18@gmail.com>
2025-03-11 21:30:00 +0500

    implement number guessing logic with input validation and feedback

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 
 use std::io;
 use rand::Rng;
@@ -5,18 +6,31 @@ fn main() {
     println!("Guess a Number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=50);
-    
-    println!("The secret number is: {secret_number}");
+    loop {
 
     println!("Please input your guess.");
 
-     let mut guess = String::new();
+    let mut guess = String::new();
 
      io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        println!("you guessed: {guess}");
+        
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+    
+        
+    println!("you guessed: {guess}");
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
 
-
-
+}
 }
